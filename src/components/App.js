7d0bd62ca1320b7740dcaff3323ece29ffe60566
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-23 14:03:01 +0200

    add one star

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,9 @@ const App = () => {
       <polygon points="235 420,224 370,213 306,221 174,257 51,329 130,464 420" fill="#362B1C" />
       <polygon points="139 205,10 420,318 420,280 359,235 255,225 180,257 51" fill="#282015" />
     </svg>
+    <svg height="390" width="550" className="star1">
+      <circle cx="5" cy="5" r="5" fill="#F7F3E3" />
+    </svg>
     <svg height="120" width="650" className="cloud1">
       <polygon points="296 207,316 195,349 205,382 230,407 210,438 199,443 167,417 152,395 132,371 141,307 136,283 187" fill="#F7F3E3" />
     </svg>
