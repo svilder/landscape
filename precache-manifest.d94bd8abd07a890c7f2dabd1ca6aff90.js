dee6d3d2595824365bf2aebd7f66f9200619bbdb
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-23 13:03:03 +0200

    Updates

---
+++ precache-manifest.d94bd8abd07a890c7f2dabd1ca6aff90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3346a387aa2cff3991bbe23d7009a98",
+    "url": "/landscape/index.html"
+  },
+  {
+    "revision": "567f43c0791aad2dc162",
+    "url": "/landscape/static/css/main.956fb594.chunk.css"
+  },
+  {
+    "revision": "c5df37f7b8b84b84b742",
+    "url": "/landscape/static/js/2.7a20e62d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landscape/static/js/2.7a20e62d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567f43c0791aad2dc162",
+    "url": "/landscape/static/js/main.bd6488b6.chunk.js"
+  },
+  {
+    "revision": "23212ed59d3c36408296",
+    "url": "/landscape/static/js/runtime-main.9a86d951.js"
+  }
+]);
