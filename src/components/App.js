b6db4dc8042719c31ecce4b1aed20a74ff17964d
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-24 12:18:31 +0200

    trees can move now

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <polygon points="235 420,224 370,213 306,221 174,257 51,329 130,464 420" fill="#362B1C" />
       <polygon points="139 205,10 420,318 420,280 359,235 255,225 180,257 51" fill="#282015" />
     </svg>
-    <svg height="80%" width="100%" className="bigstar">
+    <svg height="70%" width="80%" className="bigstar">
       <circle cx="5" cy="5" r="5" fill="#F7F3E3" />
     </svg>
     <svg viewBox="0 0 100 100" height="80%" width="100%" className="stars">
@@ -35,9 +35,9 @@ const App = () => {
     <svg height="100" width="100" className="moon">
       <circle cx="50" cy="50" r="50" fill="#F7F3E3" />
     </svg>
-    <Tree cssClass="tree1" />
-    <Tree cssClass="tree2" />
-    <Tree cssClass="tree3" />
+    <Tree cssClass="tree1" num1={36} num2={36} num3={28} num4={32} num5={20} num6={26} num7={18} />
+    <Tree cssClass="tree3" num1={40} num2={42} num3={27} num4={43} num5={15} num6={35} num7={32} />
+    <Tree cssClass="tree2" num1={38} num2={40} num3={30} num4={40} num5={25} num6={40} num7={15} />
   </div>
   );
 }
--- src/components/Tree.js
+++ src/components/Tree.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 const Tree = (props) => {
   const cssClass = props.cssClass;
+  const num1 = props.num1 + Math.round(Math.random() * 2);
+  const num2 = props.num2 + Math.round(Math.random() * 3);
+  const num3 = props.num3 + Math.round(Math.random() * 3);
+  const num4 = props.num4 + Math.round(Math.random() * 4);
+  const num5 = props.num5 + Math.round(Math.random() * 3);
+  const num6 = props.num6 + Math.round(Math.random() * 10);
+  const num7 = props.num7 + Math.round(Math.random() * 7);
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -11,13 +18,13 @@ const Tree = (props) => {
       className={cssClass}>
       <rect x="29" y="47" width="3" height="13" rx="1" fill="#1B150E"/>
       <path 
-        d='M30 50 
-          S36 46 36 40 
-          S40 36 36 28 
-          C32 20 42 14 28 4 
-          C18 18 26 24 26 30 
-          C26 36 24 40 30 50 
-          Z' 
+        d={`M30 50
+          S36 46 ${num1} 40
+          S40 36 ${num2} ${num3}
+          C${num4} ${num5} 42 14 ${num6} 4
+          C${num7} 18 26 24 26 30
+          C26 36 24 40 30 50
+          Z`} 
         fill='#56242B'/>
     </svg>
   );
